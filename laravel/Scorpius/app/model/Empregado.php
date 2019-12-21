91c6b3ecd33ecb2a6ba7ae8c6b075afdc6c737d2
Marianasls <marianalima0803@gmail.com>
2019-12-21 20:56:46 -0300

    adicionando algumas classes

---
+++ laravel/Scorpius/app/model/Empregado.php
@@ -0,0 +1,27 @@
+<?php
+
+class Empregado extends Pessoa{
+    private $permissoes = array();
+    
+    public function checkin($lista){
+
+    }
+
+    public function gerenciarVisitas(){
+
+    }
+
+    public function acessarRelatorios(){
+
+    }
+
+    public function listaVisitantes(){
+        
+    }
+
+    public function resumoSemanal(){
+
+    }
+}
+
+?>
---
+++ laravel/Scorpius/app/model/Usuario.php
@@ -0,0 +1,14 @@
+<?php
+
+class Usuario{
+    public function alterarDados($nome, $email, $telefone){}
+    
+    public function cancelarAgendamento($agendamento){}
+
+    public function notificaEmail($mensagem){}
+
+    public function confirmarAgendamento($agendamento){}
+
+}
+
+?>
