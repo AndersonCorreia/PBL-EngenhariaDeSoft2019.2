7067d1dbe80a6e88e0519b9925b2e153a4e09a81
anderson <anderson-strife@outlook.com>
2019-12-22 20:05:54 -0300

    alterando estrutura da model

--- scripts/ScriptBDScorpius.sql
+++ scripts/ScriptBDScorpius.sql
@@ -13,7 +13,7 @@ SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,N
 
 -- -----------------------------------------------------
 -- Schema scorpius
--- -----------------------------------------------------
+-- ----------------------------------------------------
 CREATE SCHEMA IF NOT EXISTS `scorpius` DEFAULT CHARACTER SET utf8 ;
 USE `scorpius` ;
 
