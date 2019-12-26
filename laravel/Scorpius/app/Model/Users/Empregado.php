57ecd2c207fe9bd3e1b09a0afac977325bbe2806
Marianasls <marianalima0803@gmail.com>
2019-12-25 23:32:41 -0300

    documentando empregado

--- laravel/Scorpius/app/Model/Users/Empregado.php
+++ laravel/Scorpius/app/Model/Users/Empregado.php
@@ -1,24 +1,53 @@
 <?php
+/**
+ * @version 1.0.0
+ */
 
 class Empregado extends Pessoa{
     private $permissoes = array();
-    
-    public function checkin($lista){
+
+    /**
+     * Método realiza confirmação de pessoas marcadas para uma visita em um turno.
+     *
+     * @param [type] $lista lista de pessoas inscritas
+     * @return void
+     */
+    public function checkin($lista){ 
 
     }
 
+    /**
+     * Gerencia e organiza visitas marcadas.
+     *
+     * @return void
+     */
     public function gerenciarVisitas(){
 
     }
 
+    /**
+     * Undocumented function
+     *
+     * @return void
+     */
     public function acessarRelatorios(){
 
     }
 
+    /**
+     * 
+     *
+     * @return void
+     */
     public function listaVisitantes(){
         
     }
 
+    /**
+     * Undocumented function
+     *
+     * @return void
+     */
     public function resumoSemanal(){
 
     }
