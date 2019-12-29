92b8108bd32a97bda68ea17c1e3fbf5fde90f17e
Kevin Gomes <38994372+Goomez@users.noreply.github.com>
2019-12-29 14:50:10 -0300

    Criacão do layout padrão das telas de login

--- laravel/Scorpius/resources/views/layouts/templateLogin.blade.php
+++ laravel/Scorpius/resources/views/layouts/templateLogin.blade.php
@@ -4,17 +4,26 @@
 -->
 
 @include('layouts._includes.top')
-<!-- Aqui ficara as coisas que ficam ao lado esquerdo das telas de login, que no caso, 
-é aquela imagem do background -->
-<div id="lado_esquerdo">
-<img id="img-esquerda" src="" alt="">
-</div>
 
-<!-- Aqui ficara as coisas que ficam ao lado direito das telas de login, como os campos
-os botões, etc.. -->
-<div id="lado_direito">
-<img id="nome-scorpius" src="" alt="">
-@yield('conteudo')
+<div class="row">
+    <!-- Aqui ficara as coisas que ficam ao lado esquerdo das telas de login, que no caso, 
+    é aquela imagem do background -->
+    
+        <div class="col-sm" id="lado_esquerdo">
+            <img width="92%" class="img-fluid" src="{{ asset('img/background-login-2.png') }}">
+        </div>
+      
+
+    <!-- Aqui ficara as coisas que ficam ao lado direito das telas de login, como os campos
+    os botões, etc.. -->
+    <div class="col-sm" id="lado_direito">
+        <div class="w-auto p-3">
+            <div class="text-center">
+            <img width="250" class="img-fluid" id="nome-scorpius" src="{{ asset('img/nome-scorpius.png') }}">
+            </div>
+            @yield('conteudo')
+        </div>
+    </div>
 </div>
 
 @include('layouts._includes.footer')
