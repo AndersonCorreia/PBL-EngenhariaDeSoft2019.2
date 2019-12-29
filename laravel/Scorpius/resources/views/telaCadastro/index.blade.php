6cc9cc096c5010ceaa02815c55d4b8081f3fc50a
Kevin Gomes <38994372+Goomez@users.noreply.github.com>
2019-12-29 14:50:10 -0300

    Criacão do layout padrão das telas de login

--- laravel/Scorpius/resources/views/telaCadastro/index.blade.php
+++ laravel/Scorpius/resources/views/telaCadastro/index.blade.php
@@ -4,18 +4,21 @@
 @section('title', 'Cadastre-se - Scorpius')
 
 @section('conteudo')
-<h1>Cadastre-se</h1>
-<h4>Já possui uma conta? <a href=""> Entre</a></h4><br>
-<div class="">
-    <button type="button" class="btn btn-md btn-fb"><i class="fab fa-facebook-f pr-1"></i>Cadastre-se com o Facebook</button>
-    
-    <button type="button" class="btn btn-gplus"><i class="fab fa-google-plus-g pr-1"></i> Cadastre-se com o Google</button>
-</div><br>
+<div class="text-center">
+    <h1>Cadastre-se</h1>
+    <h4>Já possui uma conta? <a href=""> Entre</a></h4><br>
+    <div class="">
+        <button type="button" class="btn btn-md btn-fb"><i class="fab fa-facebook-f pr-1"></i>Cadastre-se com o Facebook</button>
+        
+        <button type="button" class="btn btn-gplus"><i class="fab fa-google-plus-g pr-1"></i> Cadastre-se com o Google</button>
+    </div>
+</div>
+<br>
 <form action="" method="POST">
     <input placeholder="Nome" id="nomeCadastro" name="nome" type="text">
     <input placeholder="Sobrenome" id="sobrenomeCadastro" name="sobrenome" type="text"><br>
     <input placeholder="E-mail" id="emailCadastro" name="e-mail" type="text"><br>
-    <input type="text" class="form-control cpf-mask" placeholder="000.000.000-00">
+    <input type="text" class="" placeholder="000.000.000-00">
     <!-- Os radio buttons -->
     <div class="radio">
         <input type="radio" name="optionsRadios" id="optionsRadios1" value="option1" checked="">
