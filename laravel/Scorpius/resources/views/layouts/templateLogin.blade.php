7162b67a1087862bec04f4138ae6fa2ab0b48e09
andersonCorreia <anderson-strife@outlook.com>
2020-01-05 23:26:25 -0300

    removendo conteudo 'lixo' do layout geral, ajustando altura do layout de login

--- laravel/Scorpius/resources/views/layouts/templateLogin.blade.php
+++ laravel/Scorpius/resources/views/layouts/templateLogin.blade.php
@@ -9,17 +9,17 @@
     <!-- Aqui ficara as coisas que ficam ao lado esquerdo das telas de login, que no caso, 
     é aquela imagem do background -->
     
-        <div class="col-sm" id="lado_esquerdo">
-            <img width="92%" src="{{ asset('img/background-login-2.png') }}">
+        <div class="col-sm-6 h-100 p-0 m-0 " id="lado_esquerdo">
+            <img class="img-fluid h-100" src="{{ asset('img/background-login-2.png') }}">
         </div>
       
 
     <!-- Aqui ficara as coisas que ficam ao lado direito das telas de login, como os campos
     os botões, etc.. -->
-    <div class="col-sm-5 h-100" id="lado_direito">
-        <div class="w-auto h-50 overflow-auto p-3">
+    <div class="col-sm h-100" id="lado_direito">
+        <div class="h-100 overflow-auto p-2">
             <div class="text-center">
-            <img width="250" class="img-fluid" id="nome-scorpius" src="{{ asset('img/nome-scorpius.png') }}">
+                <img class="img-fluid w-50" id="nome-scorpius" src="{{ asset('img/nome-scorpius.png') }}">
             </div>
             @yield('conteudo')
         </div>
@@ -27,9 +27,9 @@
 </div>
 
 <style type="text/css">
-   html,
-    body,
-    .tela {
+   html, body, #tela {
+        margin: 0;
+        padding: 0;
         height: 100vh;
     }
 </style>
