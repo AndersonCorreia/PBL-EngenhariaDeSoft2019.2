38f40906383a47909348389f158655429d5be03b
Kevin Gomes <38994372+Goomez@users.noreply.github.com>
2020-01-09 01:44:20 -0300

    Menu da tela iniciado.
    A organização e disposição do menu foi iniciada.

--- laravel/Scorpius/resources/views/paginaInicial.blade.php
+++ laravel/Scorpius/resources/views/paginaInicial.blade.php
@@ -3,6 +3,7 @@
 <!--@section('title', 'Scorpius') -->
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -13,35 +14,22 @@
     <script src='https://kit.fontawesome.com/a076d05399.js'></script>
     <title>Scorpius</title>
 </head>
-<body>
-    <div id="barra-topo" class="flex-center position-ref full-heigh">
-        <div>
-            <img id="imgNomeSistema" src="{{ asset('img/tela_inicial/somente-nome-img.png') }}">
-        </div>
-        <div id="cadastrar-entrar" class="top-right" role="toolbar">
-            <div class="btn-group mr-3 ml-3" role="group">
-                <a href="{{ route('entrar') }}">
-                    <button class="btn btn-outline-success">Entrar</button>
-                </a>
-            </div>
-            <div  class="btn-group mr-3" role="group">
-                <a href="{{ route('cadastrar') }}">
-                    <button class="btn btn-success">Cadastre-se</button>
-                </a>
-            </div>
-        </div>
-    </div>
-    <!-- Menu principal -->
-    <div id="MenuPrincipal">
-        <figure class="fig-sobreposta" id="img-menu">
-            <img id="imgLogoMenu" src="{{ asset('img/tela_inicial/logo-somente-galacia-img.png') }}">
-            <a class="obj-sobreposta" id="linkAntares" href="http://www.antares.uefs.br/">
-                <i class='fas fa-external-link-alt' style='font-size:large'></i>ANTARES
-            </a>
-            <a class="obj-sobreposta" id="linkInfo" href="#sobre-observatorio">INFORMAÇÕES</a>
-        </figure>
-    </div>
 
+<body>
+
+    <div>
+        <!-- Menu principal -->
+        <div id="MenuPrincipal">
+            <figure class="fig-sobreposta" id="img-menu">
+                <img id="imgLogoMenu" src="{{ asset('img/tela_inicial/logo-somente-galacia-img.png') }}">
+                <a class="obj-sobreposta" id="linkAntares" href="http://www.antares.uefs.br/">
+                    <i class='fas fa-external-link-alt' style='font-size:large'></i>ANTARES
+                </a>
+                <a class="obj-sobreposta" id="linkInfo" href="#sobre-observatorio">INFORMAÇÕES</a>
+            </figure>
+        </div>
+    </div>
+    
     <div id="sobre-observatorio">
         <img style="float:left;" id="imgLogoMenu" src="{{ asset('img/tela_inicial/logo-somente-galacia-img.png') }}">
         <div>
@@ -57,6 +45,7 @@
         </div>
     </div>
 </body>
+
 </html>
 
 <!-- @include('layouts._includes.footer') -->
