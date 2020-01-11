12f80841a944e2d071ac1caa65b2f4cd748dd154
anderson kurosaki <anderson-strife@outlook.com>
2020-01-11 21:14:21 -0200

    alterações na tela de cadastro

--- laravel/Scorpius/resources/views/layouts/_includes/top.blade.php
+++ laravel/Scorpius/resources/views/layouts/_includes/top.blade.php
@@ -7,5 +7,6 @@
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <title>@yield('title')</title>
+    @yield('css.head')
 </head>
 <body>
