6c3070cc14f775cf2ad71a5e1523feaec68b6c54
Marianasls <marianalima0803@gmail.com>
2020-01-13 19:34:15 -0300

    alterando rotas

--- laravel/Scorpius/routes/web.php
+++ laravel/Scorpius/routes/web.php
@@ -34,9 +34,6 @@ Route::post('/cadastro/cadastrar-se', ['as'=>'cadastro.normal', 'uses'=>'Admin\C
 /**
  * Rota para a tela de instituicões de ensino dentro do escopo de usuário.
  */
-<<<<<<< HEAD
-Route::get('/instituicaoEnsino',['uses'=>'ControlerInstitucional@telaInstituicao'])->name("instituição.show");
-=======
 Route::get('/instituicaoEnsino',['uses'=>'ControlerUsuario@telaInstituicao'])->name("instituição.show");
 
 /**
@@ -48,7 +45,6 @@ Route::get('/instituicao/cadastro', 'ControlerUsuario@telaCadastroInstituicao')-
  * Rota para casdastra uma instituição e vinculala a um usuario
  */
 Route::post('/instituicao/cadastro', ['as' => 'user.instituicoes.cadastrar','uses'=>'ControlerUsuario@cadastrarInstituicao']);
->>>>>>> 1d44b8c3c058e803e3c1dca2ed1c8e507dc24da2
 
 /**
  * Rota para retornar a tela para cadastra uma instituição
