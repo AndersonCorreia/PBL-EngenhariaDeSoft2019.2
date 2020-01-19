350638329424cd2737dd474a5547aae3b3a5da95
Kevin Gomes <38994372+Goomez@users.noreply.github.com>
2020-01-19 12:33:50 -0300

    Separador de eventos criado.
    Agora o controller da pagina inicial separa os eventos da tabela "exposicao" dependedo do seu tipo (Exposição ou Atividade Diferenciada) em variáveis separadas

--- laravel/Scorpius/app/Http/Controllers/InicialController.php
+++ laravel/Scorpius/app/Http/Controllers/InicialController.php
@@ -1,32 +1,46 @@
 <?php
 // Controller das telas iniciais
 namespace App\Http\Controllers;
+
 use App\Model\Exposicao;
 use Illuminate\Http\Request;
 use App\DB\ExposicaoDAO;
 
 class InicialController extends Controller
-{   
+{
     public function inicio()
     {
-
+        
         $DAO = new ExposicaoDAO();
+        $eventos = $DAO->SELECT_ALL();
+        $exposicoes = array();
+        $atividades = array();
+        foreach ($eventos as $evento) {
+            if ($evento['Tipo_Evento'] == 'atividade permanente') {
+                $exposicoes[] = $evento;
+            } else {
+                $atividades[] = $evento;
+            }
+        }
         $variaveis = [
-            'exposicoes' => $DAO->SELECT_ALL()
+            'exposicoes' => $exposicoes,
+            'atividades' => $atividades
         ];
         return view('paginaInicial', $variaveis);
     }
     // retorna a tela de cadastro
-    public function telaCadastro(){
+    public function telaCadastro()
+    {
         return view('telaCadastro.index');
     }
     //retorna a tela de entrar
-    public function telaEntrar(){
+    public function telaEntrar()
+    {
         return view('telaEntrar.index');
     }
 
-    public function loginAdm(){
+    public function loginAdm()
+    {
         return view('loginAdministrativo.index');
     }
-
 }
