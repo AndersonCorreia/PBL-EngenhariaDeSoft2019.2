d633cae4c9933734dc1e9d99d8ddad2e519a0a4e
glardios <39931750+glardios@users.noreply.github.com>
2020-02-06 19:54:14 -0300

    atualização tela

--- laravel/Scorpius/resources/views/TelaPropostaHorarioFuncionario/telaGerenciamentoDehorarios.blade.php
+++ laravel/Scorpius/resources/views/TelaPropostaHorarioFuncionario/telaGerenciamentoDehorarios.blade.php
@@ -15,25 +15,109 @@
                 @endif
         </datalist>
     </div>
-    <div class="">
+    <div class="input-group-append">
         <h4>COMPROVANTE DE MATRÍCULA</h4>
-        <button type="button" class="btn btn-outline-secondary" >Download</button>
+        <button type="button" class="btn btn-secondary">Download</button>
     </div>
 
+    <div class="calendario">
+    <h4>CRONOGRAMA SEMANAL DO SEMESTRE</h4>
+    <table class="table table-hover">
+            <thead>
+                <tr class="table-primary">
+                    <th scope="col">&nbsp;</th>
+                    <th scope="col">Manhã</th>
+                    <th scope="col">Tarde</th>
+                    <th scope="col">Noite</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <th scope="row" class="table-secondary">Segunda</th>
+                    <td id="segundaManha"></td>
+                    <td id="segundaTarde"></td>
+                    <td id="segundaNoite"></td>
+                </tr>
+                <tr>
+                    <th scope="row" class="table-secondary">Terça</th>
+                    <td id="tercaManha"></td>
+                    <td id="tercaTarde"></td>
+                    <td id="tercaNoite"></td>
+                </tr>
+                <tr>
+                    <th scope="row" class="table-secondary">Quarta</th>
+                    <td id="quartaManha"></td>
+                    <td id="quartaTarde"></td>
+                    <td id="quartaNoite"></td>
+                </tr>
+                <tr>
+                    <th scope="row" class="table-secondary">Quinta</th>
+                    <td id="quintaManha"></td>
+                    <td id="quintaTarde"></td>
+                    <td id="quintaNoite"></td>
+                </tr>
+                <tr>
+                    <th scope="row" class="table-secondary">Sexta</th>
+                    <td id="sextaManha"></td>
+                    <td id="sextaTarde"></td>
+                    <td id="sextaNoite"></td>
+                </tr>
+            </tbody>
+        </table>
 </div>
 @endsection
 
+<script>
+
+</script>
 <style>
-    .matricula {
-        background-color: white;
+
+    h4{
+        padding: 0px 0px 10px 0px;
     }
-    .download{
-        padding-left:40px ;
+    .calendario{
+        padding:50px 0px 0px 0px ;
     }
+
+    .download {
+        padding-left: 40px;
+    }
+
     .btn {
-    background:url(img/logo-download.png) none;
-    text-indent:-9999px;
-    width:109px;
-    height:41px;
-}
+        text-align: top;
+        width: 200px;
+        height: 40px;
+        border:3px black;
+        background: transparent url(/../img/logo-download.png) no-repeat center center;
+        overflow: hidden;
+        cursor: pointer;
+        /* vai por o cursor como forma de mão ao passar por cima do botão */
+        cursor: hand;
+        /* para o IE 5.x */
+    }
+    .input-group-append{
+        padding: 0px 0px 0px 50px;
+        display: flex;
+        flex-direction:column;
+        align-items: center;
+    }
+
+    table {
+        border-collapse: separate;
+        border-spacing: 0 8px;
+        margin-top: -8px;
+        text-align: center;
+    }
+
+    td {
+        border-left-width: 0;
+        min-width: 120px;
+        height: 50px;
+    }
+
+    td:first-child {
+        border-left-width: 1px;
+    }
+
+   
 </style>
