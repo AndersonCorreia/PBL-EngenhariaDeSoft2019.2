94c783d5e5a11e11946dccef25ea1e94050f69e2
Ian Zaque Santos <ianzaque.uefs@gmail.com>
2020-02-28 12:39:27 -0300

    Alteração no menu lateral e tela de gerenciamento de eventos

--- Scorpius/resources/views/TelaGerenciamentoDeEventos/telaGerenciamentoDeEventos.blade.php
+++ Scorpius/resources/views/TelaGerenciamentoDeEventos/telaGerenciamentoDeEventos.blade.php
@@ -11,16 +11,16 @@
                 <div class = "col-10 m-0 p-0">
                     <div class = "container-fluid bg-white shadow p-3" style = "border-bottom-right-radius: 20px; 
                     border-bottom-left-radius: 20px; border-top-right-radius: 20px; border-top-left-radius: 20px; float: middle">
-                        <div class = "card"><!--caixa da lista de atividades-->
+                        <div class = "card">    <!--caixa da lista de atividades-->
                             <div class = "card-header text-white bg-primary">
-                                <h4 class = "card-title">Lista de Atividades</h4>
+                                <h4 class = "card-title">Listagem de Eventos</h4>
                             </div>
                             <div class = "card-body" style = "max-height: 200px; overflow-y: auto;">
                                 <!--aqui dentro vão ficar as informações das atividades com botões de editar e excluir atividade ao lado-->
                             </div>
-                            <div class = "card-footer bg-transparetn border-0">
-                                <button type = "button" class = "btn btn-success">CADASTRAR NOVO ATIVIDADE</button>
-                            </div>
+                                <div class = "card-footer bg-transparetn border-0">
+                                    <button type = "button" class = "btn btn-success">Cadastrar Novo Evento</button>
+                                </div>
                         </div>
                     </div>
                 </div>
