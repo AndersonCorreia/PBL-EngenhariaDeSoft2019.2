8fcaebc2649d00d0ed3e1cdc2f868df87c3cf08d
anderson kurosaki <anderson-strife@outlook.com>
2020-03-04 00:44:04 -0300

    Front para um componente no agendamento institucional e norturno

---
+++ Scorpius/resources/views/telasUsuarios/Agendamentos/_includes/agendamentos.blade.php
@@ -0,0 +1,42 @@
+@if($tipoUserLegenda["tipo"] == "institucional")
+{{-- @foreach ($agendamentos as $item) --}}
+<div class=" col-md-6 m-0 p-0 my-3 col-12 p-3">
+    <div class="col-12 px-4 row bg-white scorpius-border-shadow"> 
+        <div class="col-6 my-1 m-0 p-0">
+            <b>Data :</b> {data}
+        </div>
+        <div class="col-6 my-1 m-0 p-0">
+            <b>Turno :</b> {turno}
+        </div>
+        <hr class="bg-light col-11 linha rounded p-0 m-0">
+        <div class="col-12 m-0 my-1 p-0">
+            <b>Instituição :</b> {Instituição}
+        </div>
+        <div class="col-6 m-0 mb-1 p-0">
+            <b>Turma :</b> {Turma}
+        </div>
+        <div class="col-6 m-0 mb-1 p-0">
+            <b>Status :</b> {status}
+        </div>
+    </div>
+</div>
+{{-- @endforeach 
+@endif
+@if($tipoUserLegenda["tipo"] == "visitante")
+{{-- @foreach ($agendamentos as $item) --}}
+<div class="col-md-6 m-0 p-0 my-3 col-12 p-3">
+    <div class="col-12 px-4 row bg-white scorpius-border-shadow">
+        <div class="col-6 m-0 my-1 p-0">
+            <b>Data :</b> {data}
+        </div>
+        <div class="col-6 m-0 my-1 p-0">
+            <b>Turno :</b> {turno}
+        </div>
+        <hr class="bg-light col-11 linha rounded p-0 m-0">
+        <div class="col-6 m-0 my-1 p-0">
+            <b>Status :</b> {status}
+        </div>
+    </div>
+</div>
+{{-- @endforeach --}}
+@endif
