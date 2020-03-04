b907b171b65121d563ad9b555ca7a4922a9a0b81
glardios <weslei200912@hotmail.com>
2020-03-04 20:58:00 -0300

    correção de commit

--- Scorpius/app/Http/Controllers/AgendamentoController.php
+++ Scorpius/app/Http/Controllers/AgendamentoController.php
@@ -24,7 +24,7 @@ require_once __DIR__."/../../../resources/views/util/layoutUtil.php";
 class AgendamentoController extends Controller{   
 
     public function confirmacaoAgendamento(Request $dados){
-        $DAO = new AgendamentoDAO();
+        $DAO = new AgendamentoInstitucionalDAO();
         $DAO->confirmaAgendamento($dados->nomeTabela,$dados->status,$dados->ID);
         $DAO->contAgendamento("cont_agendamento_cancelado", $dados->user_ID);
         if($dados->nomeTabela == "agendamento_institucional"){
