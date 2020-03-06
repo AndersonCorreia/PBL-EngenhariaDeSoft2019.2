ec4d584c0f9a93087f393ae2ec7f5befc0231f6e
Esther <esthersss09@outlook.com>
2020-03-05 21:36:52 -0300

    Ajuste agendamento

--- Scorpius/resources/views/telasUsuarios/Agendamentos/_includes/agendamentos.blade.php
+++ Scorpius/resources/views/telasUsuarios/Agendamentos/_includes/agendamentos.blade.php
@@ -1,7 +1,20 @@
 @if($tipoUserLegenda["tipo"] == "institucional")
 {{-- @foreach ($agendamentos as $item) --}}
 <div class=" col-md-6 m-0 p-0 my-3 col-12 p-3">
-    <div class="col-12 px-4 row bg-white scorpius-border-shadow"> 
+    <div class="col-12 px-4 row bg-white scorpius-border-shadow">
+        <div class="col-7 m-0 my-1 p-0">
+            <div class='col-12 m-0 p-0'>
+                <b>Instituição :</b>
+            </div>
+            {Instituição}
+        </div>
+        <div class="col-5 m-0 mb-1 p-0">
+            <div class='col-12 m-0 p-0'>
+                <b>Turma :</b>
+            </div>
+            {Turma}
+        </div>
+        <hr class="bg-light col-11 linha rounded p-0 m-0">
         <div class="col-4 my-1 m-0 p-0">
             <div class='col-12 m-0 p-0'>
                 <b>Data :</b>
@@ -19,19 +32,6 @@
                 <b>Status :</b>
             </div>
             {status}
-        </div>
-        <hr class="bg-light col-11 linha rounded p-0 m-0">
-        <div class="col-12 m-0 my-1 p-0">
-            <div class='col-12 m-0 p-0'>
-                <b>Instituição :</b>
-            </div>
-            {Instituição}
-        </div>
-        <div class="col-5 m-0 mb-1 p-0">
-            <div class='col-12 m-0 p-0'>
-                <b>Status :</b>
-            </div>
-            {Turma}
         </div>
     </div>
 </div>
