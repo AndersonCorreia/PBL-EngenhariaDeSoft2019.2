a32e7f0758919f8496b774c4a1a97ff3d0222437
Kevin Gomes <38994372+Goomez@users.noreply.github.com>
2020-03-06 23:07:41 -0300

    Listagem de visitantes da tela de check-in finalizado

--- Scorpius/app/DB/CheckinDAO.php
+++ Scorpius/app/DB/CheckinDAO.php
@@ -10,7 +10,7 @@ class CheckinDAO extends \App\DB\interfaces\DataAccessObject{
         parent::__Construct("visitante");
     }
 
-    public function SELECT_VISITA()
+    public function SELECT_ALUNOS()
     {
         $result = $this->dataBase->query("SELECT * FROM visitante_institucional");
         $visitanteInst = $result->fetch_all();
@@ -18,13 +18,15 @@ class CheckinDAO extends \App\DB\interfaces\DataAccessObject{
         $visitanteInstFinal = array();
         $agendamentoChecados = array();
         foreach($visitanteInst as $visitante){
+            # 0 = ID | 1 = nome | 2 = status_checkin | 3 = idade | 4 = agendamento_ID
             if(!in_array($visitante[4], $agendamentoChecados)){
                 $agendamentoChecados[] = $visitante[4];
                 $idAgendamento = $visitante[4];
-                $result = $this->dataBase->query("SELECT visita FROM agendamento_institucional WHERE ID = {$idAgendamento}");
+                $result = $this->dataBase->query("SELECT visita, professor_instituicao_ID FROM agendamento_institucional WHERE ID = {$idAgendamento} AND status = 'confirmado'");
                 $row = $result->fetch_assoc();
                 $idVisita = $row['visita'];
-                $result = $this->dataBase->query("SELECT data_visita, turno FROM visita WHERE ID = {$idVisita} AND status = 'confirmado'");
+                $idProfessorInstituicao = $row['professor_instituicao_ID'];
+                $result = $this->dataBase->query("SELECT data_visita, turno FROM visita WHERE ID = {$idVisita} AND status = 'não realizada'");
                 if($result->num_rows > 0){
                     $row = $result->fetch_assoc();
                     $dataVisita = $row['data_visita'];
@@ -35,16 +37,63 @@ class CheckinDAO extends \App\DB\interfaces\DataAccessObject{
                             $alunos[] = $aluno;
                         }
                     }
+                    $result = $this->dataBase->query("SELECT instituicao_ID, usuario_ID FROM professor_instituicao WHERE ID = $idProfessorInstituicao");
+                    $row = $result->fetch_assoc();
+                    $IDinstituicao = $row['instituicao_ID'];
+                    $IDprofessor = $row['usuario_ID'];
+                    $result = $this->dataBase->query("SELECT nome FROM usuario WHERE ID = $IDprofessor");
+                    $row = $result->fetch_assoc();
+                    $professor = $row['nome'];
+                    $result = $this->dataBase->query("SELECT nome FROM instituicao WHERE ID = $IDinstituicao");
+                    $row = $result->fetch_assoc();
+                    $instituicao = $row['nome'];
+                    
                     $visitanteInstFinal[] = [
                         'agendamento_ID' => $idAgendamento,
                         'data' => $dataVisita,
                         'turno' => $turno,
+                        'professor' => ['ID'=> $IDprofessor, 'nome'=> $professor],
+                        'instituicao' => ['ID'=> $IDinstituicao, 'nome'=> $instituicao],
                         'aluno' => $alunos
                     ];
                 }
             }
         }
         return $visitanteInstFinal;
+    }
+    public function SELECT_USUARIOS()
+    {
+        $result = $this->dataBase->query("SELECT * FROM visitante");
+        $visitanteInst = $result->fetch_all();
+
+        $visitanteInstFinal = array();
+        // $agendamentoChecados = array();
+        foreach($visitanteInst as $visitante){
+            # 0 = ID | 1 = nome | 2 = status_checkin | 3 = idade | 4 = RG| 5 = agendamento_ID
+            // if(!in_array($visitante[5], $agendamentoChecados)){
+                // $agendamentoChecados[] = $visitante[5];
+                $idAgendamento = $visitante[5];
+                $result = $this->dataBase->query("SELECT visita FROM agendamento WHERE ID = {$idAgendamento} AND status = 'confirmado'");
+                $row = $result->fetch_assoc();
+                $idVisita = $row['visita'];
+                $result = $this->dataBase->query("SELECT data_visita, turno FROM visita WHERE ID = {$idVisita} AND status = 'não realizada'");
+                if($result->num_rows > 0){
+                    $row = $result->fetch_assoc();
+                    // dd($row);
+                    $dataVisita = $row['data_visita'];
+                    $turno = $row['turno'];
+                    $usuario = $visitante;
+                    $visitanteInstFinal[] = [
+                        'agendamento_ID' => $idAgendamento,
+                        'data' => $dataVisita,
+                        'turno' => $turno,
+                        'usuario' => $usuario
+                    ];
+                // }
+            }
+        }
+        return $visitanteInstFinal;
+
     }
 
     public function INSERT(object $object): bool{
--- Scorpius/routes/administrativo.php
+++ Scorpius/routes/administrativo.php
@@ -70,6 +70,7 @@ Route::group(
     }
 );
 
+Route::get('/checkin-visitas', 'CheckinVisitasController@index')->name("checkinVisitas");
 Route::group(
     ['midlleware'=>[],
     'prefix'=>'estagiario',
