23d0179ade5fbf58efdbffefa456d84fcfeb6d52
andersonCorreia <anderson-strife@outlook.com>
2020-03-10 01:08:13 -0300

    Refatoração do dashboard de visitante para melhorar a o codigo e a responsividade

--- Scorpius/resources/views/Dashboard_visitante/Dashboard_visitante.blade.php
+++ Scorpius/resources/views/Dashboard_visitante/Dashboard_visitante.blade.php
@@ -8,9 +8,6 @@
         .notificacoes_agendamentos{
             padding: 10px ;
         }
-        .notificacoes{
-            
-        }
         .scroll{
             max-height: 300px;
             overflow-y: auto;
@@ -30,43 +27,35 @@
             border-top-right-radius: 20px;border-top-left-radius: 20px;
         }
     </style>
-    
-    <div class="container-fluid bg-white p-4" method="POST" action="{{route('confirma.post')}}" style="border-bottom-right-radius: 20px; 
-    border-bottom-left-radius: 20px;border-top-right-radius: 20px;border-top-left-radius: 20px">
-        <div class="col-12 m-0 p-0">
-            <div class="container-fluid bg-white shadow p-3" style="border-bottom-right-radius: 20px; 
-            border-bottom-left-radius: 20px;border-top-right-radius: 20px;border-top-left-radius: 20px">
-            <div class = "row notificacoes_agendamentos">
-                    <div class = "col-md-5 notificacoes">   <!-- DIV DAS NOTIFICAÇOES  -->
-                        <div class = "card">
-                            <div class = "card-header text-white bg-primary">
-                                <h4 class="card-title">Suas Notificações</h4>
-                            </div>
-                            <div class = "card-body scroll">                     
-                                @foreach($notificacoes as $notificacao)
-                                    <h6 style="padding: 0px 0px 20px 0px"> {{$notificacao['Mensagem']}} </h6>
-                                @endforeach
-                            </div>
-                        </div>
-                    </div>
-                    <div class = "col-md-7 agendamentos">   <!-- DIV DOS AGENDAMENTOS  -->
-                        <div class = "card">
-                            <div class = "card-header text-white bg-primary">
-                                <h4 class="card-title">Seus Agendamentos</h4>
-                            </div>
-                            <div class = "card-body scroll">                        
-                                @include('Dashboard_visitante._includes.agendamentos')
-                            </div>
-                        </div>
-                    </div>
+<div class="col overflow-auto scorpius-border-shadow-sm  mb-3 p-md-3 p-1" >
+    <div class = "row col notificacoes_agendamentos">
+        <div class = "col-md-5 col notificacoes">   <!-- DIV DAS NOTIFICAÇOES  -->
+            <div class = "card">
+                <div class = "card-header text-white bg-primary">
+                    <h4 class="card-title">Suas Notificações</h4>
+                </div>
+                <div class = "card-body scroll">                     
+                    @foreach($notificacoes as $notificacao)
+                        <h6 style="padding: 0px 0px 20px 0px"> {{$notificacao['Mensagem']}} </h6>
+                    @endforeach
                 </div>
             </div>
         </div>
-        <div class="container-fluid shadow p-4" style="border-bottom-right-radius: 20px; 
-            border-bottom-left-radius: 20px;border-top-right-radius: 20px;border-top-left-radius: 20px">
-            <div class = "row">
-                @include('telasUsuarios.Agendamentos._includes.calendar')  
+        <div class = "col-md-7 col agendamentos">   <!-- DIV DOS AGENDAMENTOS  -->
+            <div class = "card">
+                <div class = "card-header text-white bg-primary">
+                    <h4 class="card-title">Seus Agendamentos</h4>
+                </div>
+                <div class = "card-body scroll">                        
+                    @include('Dashboard_visitante._includes.agendamentos')
+                </div>
             </div>
         </div>
     </div>
+</div>
+<div class="scorpius-border-shadow-sm p-4">
+    <div class = "row">
+            @include('telasUsuarios.Agendamentos._includes.calendar')  
+    </div>
+</div>
 @endsection
