57e53ac47ba67be7a9ea44b2deb0323a78a65154
Anderson correia <40068482+AndersonCorreia@users.noreply.github.com>
2020-04-03 19:56:34 -0300

    Atualizando empregado
    Removendo redundâncias

--- Scorpius/app/Model/Users/Empregado.php
+++ Scorpius/app/Model/Users/Empregado.php
@@ -7,33 +7,12 @@ namespace App\Model\Users;
 use App\DB\PessoaDAO;
 
 class Empregado extends Pessoa{
-    private $permissoes = array();
-    private $demandaWeb = [
-        'guia',
-        'horarios',
-        'observacao'
-    ];
+   
     private $horario_estagiario;
-    
-    public function getDemandaWeb()
-    {
-        return $this->demandaWeb;
-    }
-
-    public function setDemandaWeb($demandaWeb)
-    {
-        $this->setAlterado();
-        $this->demandaWeb = $demandaWeb;
-    }
 
     public function novaDemandaWeb($id, $demandaWeb){
         $this->horario_estagiario = new PessoaDAO();
         return $this->horario_estagiario->INSERT_horario($id, $demandaWeb);
-    }
-
-    public function setTipo(string $tipo){
-        $this->setAlterado();
-        $this->tipo = $tipo;
     }
 }
 ?>
